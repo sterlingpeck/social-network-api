84060af51eb27a3f775fc639ab20733b806e0d88
Sterling Peck <peck.sterling@gmail.com>
2022-08-25 15:45:54 -0600

    getuserid, thought route

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -10,6 +10,28 @@ const userController = {
       });
   },
 
+  getUserByID({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({
+            message: "No user found with this id.",
+          });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
   createUser({ body }, res) {
     User.create(body)
       .then((dbUserData) => res.json(dbUserData))
